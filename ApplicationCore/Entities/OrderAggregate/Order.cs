5ec76a62406d17f2313993b07bc586862a1ac03a
Manish946 <manishestha@gmail.com>
2022-06-13 20:31:50 +0200

    Order entities updated.

---
+++ ApplicationCore/Entities/OrderAggregate/Order.cs
@@ -0,0 +1,43 @@
+﻿using ApplicationCore.Interfaces;
+using Ardalis.GuardClauses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities.OrderAggregate
+{
+    public class Order : BaseEntity, IAggregateRoot
+    {
+        public string BuyerId { get; private set; }
+        public DateTimeOffset OrderDate { get; private set; } = DateTimeOffset.Now;
+        public Address ShipToAddress { get; private set; }
+
+        // DDD Patterns Comment
+        // Using a private collection field, better for DDD Aggregate's encapsulation
+        // so OrderItems cannot be added from "outside the AggregateRoot" directly to the collection,
+        // but only through the method Order.AddOrderItem() which includes behavior.
+        private readonly List<OrderItem> _orderItems = new List<OrderItem>();
+
+        // Using List<>.AsReadOnly()
+        // This will create a read only wrapper around the private list so is protected against "external updates".
+        // It's much cheaper than .ToList() because it will not have to copy all items in a new collection. ( Just one heap alloc for the wrapper instance)
+
+        public Order()
+        {
+            // required by EF
+        }
+
+        public Order(string buyerId, Address shipToAddress, List<OrderItem> items)
+        {
+            Guard.Against.NullOrEmpty(buyerId, nameof(buyerId));
+            Guard.Against.Null(shipToAddress, nameof(shipToAddress));
+            Guard.Against.Null(items,nameof(items));
+
+            BuyerId = buyerId;
+            ShipToAddress = shipToAddress;
+            _orderItems = items;
+        }
+    }
+}
